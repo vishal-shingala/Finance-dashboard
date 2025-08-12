e1b77c0ff0f347e3f74c4d81b7c111fd0881927a
Vishal Shingala <vishalshingala1826@gmail.com>
2025-08-12 17:36:51 +0530

    feat: Update routing logic to conditionally render Dashboard and Profile based on user authentication

--- Frontend/src/Routes/Routes.jsx
+++ Frontend/src/Routes/Routes.jsx
@@ -3,14 +3,17 @@ import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import Register from '../pages/Register';   
+import { useContext } from 'react';
+import { UserContext } from '../context/user.context';
 
 function PageRouters(){
+    const {currUser} = useContext(UserContext);
     return(
         <BrowserRouter>
         <Routes>
             <Route path='/login' element={<Login/>}/>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/profile' element={<Profile/>}/> 
+            <Route path='/' element={currUser ? <Dashboard/> : <Login/>}/>
+            <Route path='/profile' element={currUser ? <Profile/> : <Login/>}/> 
             <Route path='/register' element={<Register/>}/> 
         </Routes>
         </BrowserRouter>
