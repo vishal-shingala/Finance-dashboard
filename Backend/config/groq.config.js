ab2f6cadc0f045114f30a3e25bfd86e4586ba80e
VishalShingala <vishalshingala1826@gmail.com>
2026-01-10 19:31:52 +0530

    Update groq.config.js

--- Backend/config/groq.config.js
+++ Backend/config/groq.config.js
@@ -2,7 +2,7 @@ import { ChatGroq } from "@langchain/groq";
 
 const llm = new ChatGroq({
   model: "openai/gpt-oss-120b",
-  apiKey: "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",
+  apiKey: "groq_api_key",
 });
 
 export default llm;
